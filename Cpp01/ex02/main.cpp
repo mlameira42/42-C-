af4b08b3075f32ca76fb7bc21e599968de6463c5
mlameira <mlameira@student.42lisboa.com>
2025-08-20 13:43:02 +0000

    finished

--- Cpp01/ex02/main.cpp
+++ Cpp01/ex02/main.cpp
@@ -7,7 +7,7 @@ int main()
 	std::string *stringPTR = &brain;
 	std::string &stringREF = brain; 
 
-	std::cout << &brain << "\t|" << &stringPTR << "\t|" << &stringREF << "\n";
+	std::cout << &brain << "\t|" << stringPTR << "\t|" << &stringREF << "\n";
 	std::cout << brain << "\t|" << *stringPTR << "\t|" << stringREF << "\n";
 	return 0;
 }
